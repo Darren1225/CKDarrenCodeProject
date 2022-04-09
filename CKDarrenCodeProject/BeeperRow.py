71e72862202999f048fb685e3e14da6e3430dce0
Darren1225 <103312520+Darren1225@users.noreply.github.com>
2022-04-09 19:18:56 +0800

    Update BeeperRow.py

--- CKDarrenCodeProject/BeeperRow.py
+++ CKDarrenCodeProject/BeeperRow.py
@@ -15,28 +15,7 @@ def main():
         put_beeper()
         move()
     put_beeper()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-####### DO NOT EDIT CODE BELOW THIS LINE ########
-
+    
+    
 if __name__ == '__main__':
     execute_karel_task(main)
-    git rm -r --cached .
-git add .
-git commit
-git push  -u origin master
